f23a147618aadc1b8afa7952195ae8768d63a45a
arnasvid <arnas.vidziunas@gmail.com>
2022-12-05 14:29:34 +0200

    Add login (semi veikia. Reikia prijungt su backu, isskirt login ir register ir t.t.)

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,14 @@
-import { Toolbar, Typography } from "@mui/material";
+import { Button, Toolbar, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
+import PersonIcon from '@mui/icons-material/Person';
 import "./Common.css";
+import LoginDialog from "components/login/LoginDialog";
+import { useState } from "react";
 
 const Header = () => {
+	const [open, setOpen] = useState<boolean>(false);
+	const handleOpen = () => setOpen(true);	
+	const handleClose = () => setOpen(false);
 	return (
 		<AppBar
 			position="static"
@@ -39,7 +45,26 @@ const Header = () => {
 				>
 					<Typography variant="h6">Header</Typography>
 				</div>
+				<div
+					style={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "flex-start",
+						width: "100%",
+					}}
+				>	
+					<Button variant="contained" color="primary" startIcon={<PersonIcon />}
+					onClick={handleOpen}
+					style={{
+						marginLeft: "auto",
+						marginRight: "0px",
+					}}
+					>
+						<Typography variant="body">Prisijungti</Typography>
+					</Button>
+				</div>
 			</Toolbar>
+			<LoginDialog open={open} handleClose={handleClose}/>
 		</AppBar>
 	);
 };
--- src/components/login/LoginDialog.tsx
+++ src/components/login/LoginDialog.tsx
@@ -1,4 +1,10 @@
-import { Button, Dialog, Typography } from "@mui/material";
+import { Box, Button, Dialog, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
+import HowToRegOutlinedIcon from "@mui/icons-material/HowToRegOutlined";
+
+import "./Login.css";
+import { borderRadius } from "@mui/system";
 
 interface LoginDialogProps {
 	open: boolean;
@@ -6,12 +12,129 @@ interface LoginDialogProps {
 }
 
 const LoginDialog = (props: LoginDialogProps) => {
+	const [isSignup, setIsSignup] = useState(false);
+	const [inputs, setInputs] = useState({
+		name: "", // only used for signup
+		surname: "", // only used for signup
+		email: "",
+		password: "",
+	});
+	const handleChange = (e) => {
+		setInputs((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(inputs);
+	};
+	const resetState = () => {
+		setIsSignup(!isSignup);
+		setInputs({
+			name: "",
+			surname: "",
+			email: "",
+			password: "",
+		});
+	};
+
 	return (
-		<Dialog open={props.open} onClose={props.handleClose}>
-			<div>
-				<Button variant="contained" color="primary">
-					<Typography variant="body">Login</Typography>
-				</Button>
+		<Dialog
+			open={props.open}
+			onClose={props.handleClose}
+			style={{
+				borderRadius: "5px",
+			}}
+		>
+			<div className="dialogastemp">
+				<form onSubmit={handleSubmit}>
+					<Box
+						display="flex"
+						flexDirection={"column"}
+						maxWidth={400}
+						alignItems="center"
+						justifyContent={"center"}
+						margin={"auto"}
+						// marginTop={5}
+						padding={3}
+						borderRadius={5}
+						// boxShadow={"5px 5px 10px #ccc"}
+						sx={{
+							":hover": {
+								boxShadow: "10px 10px 20px #ccc",
+							},
+						}}
+					>
+						<Typography variant="h2" padding={3} textAlign="center">
+							{isSignup ? "Sign Up" : "Login"}
+						</Typography>
+						{isSignup && (
+							<TextField
+								onChange={handleChange}
+								name="name"
+								value={inputs.name}
+								margin="normal"
+								type="text"
+								variant="outlined"
+								placeholder="Name"
+								required
+							/>
+						)}
+						{isSignup && (
+							<TextField
+								onChange={handleChange}
+								name="surname"
+								value={inputs.surname}
+								margin="normal"
+								type="text"
+								variant="outlined"
+								placeholder="Surname"
+								required
+							/>
+						)}
+						<TextField
+							onChange={handleChange}
+							name="email"
+							value={inputs.email}
+							margin="normal"
+							type="email"
+							variant="outlined"
+							placeholder="Email"
+							required
+						/>
+						<TextField
+							onChange={handleChange}
+							name="password"
+							value={inputs.password}
+							margin="normal"
+							type="password"
+							variant="outlined"
+							placeholder="Password"
+							required
+						/>
+						<Button
+							endIcon={
+								isSignup ? <HowToRegOutlinedIcon /> : <LoginOutlinedIcon />
+							}
+							type="submit"
+							sx={{ marginTop: 3, borderRadius: 3 }}
+							variant="contained"
+							color="warning"
+						>
+							{isSignup ? "Sign Up" : "Login"}
+						</Button>
+						<Button
+							endIcon={
+								isSignup ? <LoginOutlinedIcon /> : <HowToRegOutlinedIcon />
+							}
+							onClick={resetState}
+							sx={{ marginTop: 3, borderRadius: 3 }}
+						>
+							Change to {isSignup ? "Login" : "Signup"}
+						</Button>
+					</Box>
+				</form>
 			</div>
 		</Dialog>
 	);
