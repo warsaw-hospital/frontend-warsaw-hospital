49daf9925e01c4914ca037967104bca914b9588d
KerniusSur <kernius.survila@gmail.com>
2022-12-07 00:52:03 +0200

    WHO-11 axios interceptors update

--- src/redux/setupAxiosInterceptors.ts
+++ src/redux/setupAxiosInterceptors.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 /**
  * It intercepts all requests  responses from the axios library and if there's an error, it
@@ -6,13 +6,15 @@ import axios from "axios";
  * @param {any} store - any - this is the Redux store that we'll use to dispatch actions.
  */
 const setupAxiosInterceptors = () => {
-	// const handleResponseError = (error: AxiosError) => {
-	// 	store.dispatch(alertActions.addAlert(mapErrorToAlert(error)));
-	// 	return Promise.reject(error);
-	// };
+	const handleResponseError = (error: AxiosError) => {
+		return Promise.reject(error);
+	};
 
 	/* It's intercepting all responses and if there's an error, it calls the handleResponseError function. */
-	axios.interceptors.response.use((response) => response.data);
+	axios.interceptors.response.use(
+		(response) => response.data,
+		handleResponseError
+	);
 };
 
 export default setupAxiosInterceptors;
